e6eef025e80758aaeb5cb5d9424be6ea49887e55
Amalia-Vasquez <aivb00016@ce.pucmm.edu.do>
2023-05-18 12:52:57 -0400

    "prueba de arreglo"

--- prueba 2.c
+++ prueba 2.c
@@ -3,7 +3,14 @@
 
 int main () {
 
-    printf("Hola mundo!");
+    int arr[10];
+
+    for (int i = 0; i <10 ; i++)
+    {
+        arr[i] = i+1;
+        printf("%d\n",arr[i]);
+    }
+    
 
     return 0;
 }
