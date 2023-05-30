bd5a71f6d8330f90ec8059abac762e3ac466207e
Amalia-Vasquez <aivb00016@ce.pucmm.edu.do>
2023-05-30 16:03:01 -0400

    valores despues del punto

--- problema 12.4.c
+++ problema 12.4.c
@@ -1,8 +1,9 @@
-Hacer un programa con el que se pueda evaluar las funciones f(x), g(x) y z(x) para todos los valores de x en el intervalo 0<=x<3.5 con incremento de 0.2 Escribir un array de punteros a funcion. en lenguaje c y con comentarios
+/*Hacer un programa con el que se pueda evaluar las funciones f(x), g(x) y z(x) para todos los valores de x en 
+el intervalo 0<=x<3.5 con incremento de 0.2 Escribir un array de punteros a funcion. en lenguaje c y con comentarios
 Las funciones son las siguientes:
 f(x)= 3*e^x-2x
 g(x)=-x*sin(x)+1.5
-z(x)=x^2-2x+3
+z(x)=x^2-2x+3*/
 
 #include <stdio.h> 
 #include <math.h> 
@@ -18,10 +19,10 @@ int main()
     //Ciclo para evaluar las funciones entre 0 y 3.5 con incremento de 0.2 
 	float x;
 	for(x=0.0; x<3.5; x=x+0.2){
-		printf("Valor de x = %f\n",x); 
-		printf("Valor de f(x) = %f\n",funciones[0](x)); 
-		printf("Valor de g(x) = %f\n",funciones[1](x));
-		printf("Valor de z(x) = %f\n",funciones[2](x));
+		printf("Valor de x = %1f\n",x); 
+		printf("Valor de f(x) = %1f\n",funciones[0](x)); 
+		printf("Valor de g(x) = %1f\n",funciones[1](x));
+		printf("Valor de z(x) = %1f\n",funciones[2](x));
         printf("\n");
 	}
 	return 0; 
